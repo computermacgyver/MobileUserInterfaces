8e5b97d5b9beac6643c0795a98f73a604e2ef4d4
ironholds <ironholds@gmail.com>
2015-01-28 01:07:24 -0500

    add directory creation

--- main.R
+++ main.R
@@ -4,6 +4,8 @@ source("retrieve.R")
 source("analyse.R")
 
 #Create necessary directories
+dir.create(file.path(getwd(),"Paper","Figures"), showWarnings = FALSE)
+dir.create(file.path(getwd(),"Paper","Datasets"), showWarnings = FALSE)
 
 #Run!
 analyse(retrieve())
